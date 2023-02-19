0b59123980219cb7b57078cf7cabd89d391c503d
orib <oribrau@gmail.com>
2023-02-19 14:16:42 +0200

    added user dashboard api added company domain to detect company email login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,25 +2,29 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/
 import {environment} from "../../../environments/environment";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('videoplayer') videoplayer!: ElementRef;
-  videoUrl = environment.staticUrl + 'assets/videos/loop_video..mp4';
-  title = 'frontend';
-  staticServerPath = '';
-  constructor() {}
+    @ViewChild('videoplayer') videoplayer!: ElementRef;
+    videoUrl = environment.staticUrl + 'assets/videos/loop_video..mp4';
+    imageUrl = environment.staticUrl + 'assets/images/Artificial-Intelligence-Trends-scaled-1.jpeg';
+    logoUrl = environment.staticUrl + 'assets/images/Gaia_blue_long_transparent.png';
+    title = 'frontend';
+    staticServerPath = '';
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  ngAfterViewInit(): void {
-    const media = this.videoplayer.nativeElement;
-    media.muted = true; // without this line it's not working although I have "muted" in HTML
-    media.play();
-  }
+    ngAfterViewInit(): void {
+        if (this.videoplayer) {
+            const media = this.videoplayer.nativeElement;
+            media.muted = true; // without this line it's not working although I have "muted" in HTML
+            media.play();
+        }
+    }
 
 }
