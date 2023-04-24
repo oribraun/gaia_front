3417635c6a8a5d9ceb2464a13560c342f7c9bc20
orib <oribrau@gmail.com>
2023-04-24 19:17:32 +0300

    added option to change pre prompt

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -3,6 +3,7 @@ import {User} from "../../entities/user";
 import {Config} from "../../config";
 import {ApiService} from "../../services/api.service";
 import {lastValueFrom} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Component({
     selector: 'app-settings',
@@ -13,8 +14,9 @@ export class SettingsComponent implements OnInit {
 
     user!: User;
     company: any;
-    errMessage = ''
-    successMessage = ''
+    errMessage = '';
+    successMessage = '';
+    howToImplement = '';
     constructor(
         private config: Config,
         private apiService: ApiService
@@ -35,17 +37,44 @@ export class SettingsComponent implements OnInit {
                 this.errMessage = 'Company do not exist';
             } else {
                 this.company = response.data;
+                this.setHowToImplement();
             }
         } else {
             this.errMessage = response.errMessage;
         }
     }
 
+    setHowToImplement() {
+        this.howToImplement = `function postData(url = '', data = {}, token = '') {
+    return fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json());
+}
+
+token = '${this.company.open_ai_key}'
+data = {prompt: 'Hi', stream: false, conversation_id: 'some_conversation_id'}
+url = '${environment.serverUrl}api/ct/smart-router'
+postData(url, data, token)
+`;
+    }
+
     async saveCompanySettings() {
         this.resetMessages();
         const obj = {
             id: this.company.id,
-            open_ai_key: this.company.open_ai_key
+            open_ai_key: this.company.open_ai_key,
+            pre_prompt: this.company.pre_prompt
         }
         const response: any = await lastValueFrom(this.apiService.setCompany(obj));
         if (!response.err) {
