6a0c378838cd09bdd5e2f473ac2a0d3876545a1e
oribraun <oribrau@gmail.com>
2023-11-23 17:44:59 +0200

    fixed Interceptor

--- src/app/platforms/main/interceptors/my-http-interceptor.ts
+++ src/app/platforms/main/interceptors/my-http-interceptor.ts
@@ -3,7 +3,7 @@ import {
     HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {catchError, Observable, tap, throwError} from 'rxjs';
 import {Router} from "@angular/router";
 import {Config} from "../config";
 
@@ -18,36 +18,48 @@ export class MyHttpInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('req', req)
-        const request = next.handle(req);
-        request.subscribe({
-            next: (response: any) => {
-                // console.log('MyHttpInterceptor response1', response)
-                // console.log('MyHttpInterceptor response1 body', response.body)
-                // console.log('MyHttpInterceptor response1 err', response.body.err)
-                // console.log('MyHttpInterceptor response1 errMessage', response.body.errMessage)
-                if (response.body && response.body.err && response.body.errMessage === 'Unauthorized platform') { // user has no platform
+        return next.handle(req).pipe(
+            tap({
+                next: (event: any) => {
+                    if (event instanceof HttpResponse) {
+                        // console.log('MyHttpInterceptor Response:', event);
+                        // console.log('MyHttpInterceptor response body', event.body)
+                        // console.log('MyHttpInterceptor response err', event.body.err)
+                        // console.log('MyHttpInterceptor response errMessage', event.body.errMessage)
+                        const body = event.body;
+                        if (body && body.err && body.errMessage === 'Unauthorized platform') { // user has no platform
 
+                        }
+                        if (body && body.err && body.errMessage === 'Unauthorized') { // user not logged in
+                            // user platfrom is wrong or user cookie expired
+                            const queryParams: any = {}
+                            queryParams['authType'] = 'login';
+                            // this.router.navigate(['/'], { queryParams: queryParams})
+                        }
+                    }
+                },
+                error: (error: any) => {
+                    if (error instanceof HttpErrorResponse) {
+                        console.error('MyHttpInterceptor HTTP error:', error);
+                    }
                 }
-                if (response.body && response.body.err && response.body.errMessage === 'Unauthorized') { // user not logged in
-                    // user platfrom is wrong or user cookie expired
-                    const queryParams: any = {}
-                    queryParams['authType'] = 'login';
-                    // this.router.navigate(['/'], { queryParams: queryParams})
+            }),
+            catchError((error: any) => {
+                if (error instanceof HttpErrorResponse && error.status === 401) {
+                    this.redirectToLogin(true);
+                }
+                return throwError(error);
+            })
+        )
+    }
 
-                }
-            },
-            error: (error: any) => {
-                // console.log('MyHttpInterceptor error', error)
-                if (error instanceof HttpErrorResponse && (error as HttpErrorResponse).status === 401) {
-                    // console.log('MyHttpInterceptor 401')
-                    const queryParams: any = {}
-                    queryParams['authType'] = 'login';
-                    this.config.resetCookies();
-                    this.config.resetUserCreds();
-                    this.router.navigate(['/'], { queryParams: queryParams})
-                }
-            }
-        });
-        return request
+    redirectToLogin(resetConf=false) {
+        const queryParams: any = {}
+        queryParams['authType'] = 'login';
+        if (resetConf) {
+            this.config.resetCookies();
+            this.config.resetUserCreds();
+        }
+        this.router.navigate(['/'], { queryParams: queryParams})
     }
 }
