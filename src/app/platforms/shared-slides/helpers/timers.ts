9bf4dc53d689f7a88141d5fa99c854dd8d73a3e4
oribraun <oribrau@gmail.com>
2023-12-07 11:24:40 +0200

    added mark time as red when less then 60 seconds

--- src/app/platforms/shared-slides/helpers/timers.ts
+++ src/app/platforms/shared-slides/helpers/timers.ts
@@ -1,11 +1,37 @@
+interface Timers {
+    [key: number]: Timer;
+}
+
+interface Counters {
+    [key: number]: Timer;
+}
+
+class Timer {
+    active!: boolean;
+    counter!: number;
+    minutes!: number;
+    minutesStr!: string;
+    seconds!: number;
+    secondsStr!: string;
+    submited!: boolean;
+    intervalId: any;
+    getTime = () => {
+        return this.minutesStr + ':'+this.secondsStr
+    }
+
+    public constructor(init?:Partial<Timer>) {
+        Object.assign(this, init);
+    }
+}
+
 
 export class TimersHelper {
 
-    timers:any = {}
-    counters:any = {}
+    timers:Timers = {}
+    counters:Counters = {}
     current_counter:any = {}
     current_timer:any = {}
-    
+
     constructor() { }
 
     clearAllCounters(){
@@ -28,7 +54,7 @@ export class TimersHelper {
         this.current_counter = this.counters[counter_idx]
     }
 
-    handleTimer(timer_idx:number, initial_value:number=0, pause_others=true){
+    handleTimer(timer_idx:number, initial_value:number=0, pause_others=true) {
         if(pause_others){
             this.pauseAllTimers()
         }
@@ -40,21 +66,21 @@ export class TimersHelper {
         this.current_timer = this.timers[timer_idx]
     }
 
-    createTimer(type:string='counter', initial_value:number=0){
-        let Timer = Object()
-        Timer.active = true
-        
-        Timer.counter = initial_value
-        Timer.minutes = Math.floor(initial_value/60)
-        Timer.minutesStr = Timer.minutes.toString().length < 2 ? '0' + Timer.minutes: Timer.minutes
-        Timer.seconds = Timer.counter%60
-        Timer.secondsStr = Timer.seconds.toString().length < 2 ? '0' + Timer.seconds: Timer.seconds
-        Timer.submited = false
-        Timer.intervalId = setInterval(this.progressTimer, 1000,Timer, type);
-        Timer.getTime = function(){
+    createTimer(type:string='counter', initial_value:number=0): Timer {
+        let timer = new Timer()
+        timer.active = true
+
+        timer.counter = initial_value
+        timer.minutes = Math.floor(initial_value/60)
+        timer.minutesStr = (timer.minutes.toString().length < 2 ? '0' + timer.minutes: timer.minutes).toString()
+        timer.seconds = timer.counter%60
+        timer.secondsStr = (timer.seconds.toString().length < 2 ? '0' + timer.seconds: timer.seconds).toString()
+        timer.submited = false
+        timer.intervalId = setInterval(this.progressTimer, 1000,timer, type);
+        timer.getTime = function(){
             return this.minutesStr + ':'+this.secondsStr
         }
-        return Timer
+        return timer
 
     }
 
@@ -72,7 +98,7 @@ export class TimersHelper {
 
     progressTimer(self:any, type:string='counter') {
         if (self.active && !self.submited){
-            self.counter= type=='counter'? self.counter+1 : self.counter-1 
+            self.counter= type=='counter'? self.counter+1 : self.counter-1
             self.minutes = Math.floor(self.counter/60)
             self.minutesStr = self.minutes.toString().length < 2 ? '0' + self.minutes: self.minutes
             self.seconds = self.counter%60
