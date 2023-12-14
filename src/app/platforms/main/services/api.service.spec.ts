cb714085b51bdf036ca1e2351815fe26cfe5923a
oribraun <oribrau@gmail.com>
2023-12-14 14:46:43 +0200

    started adding tests

--- src/app/platforms/main/services/api.service.spec.ts
+++ src/app/platforms/main/services/api.service.spec.ts
@@ -1,21 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 
 import { ApiService } from './api.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {Config} from "../config";
 
 describe('ApiService', () => {
-  let service: ApiService;
+    let api_service: ApiService;
+    let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        providers: [Config]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [Config]
+        });
+        api_service = TestBed.inject(ApiService);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
-    service = TestBed.inject(ApiService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    afterEach(() => {
+        // After each test, verify that there are no outstanding HTTP requests
+        httpTestingController.verify();
+    });
+
+    it('getPlatforms', fakeAsync(() => {
+        api_service.getPlatforms({}).subscribe((result: any) => {
+            // Check that the response matches the expected data
+            expect(result.platforms).toEqual([]);
+        });
+
+        const url = api_service.serverBase + api_service.baseApiPublic + 'get-platforms';
+        const req = httpTestingController.expectOne({
+            method: 'POST',
+            url: `${url}`
+        });
+
+        req.flush({platforms: []});
+    }));
+
+    it('should be created', () => {
+        expect(api_service).toBeTruthy();
+    });
 });
